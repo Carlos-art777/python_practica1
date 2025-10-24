b49f162b1ee74375501b0dbb6f88f161b90f40ff
carlos-art777 <arlequin1535@hotmail.com>
2025-10-23 23:14:17 -0300

    Validacion del numero ingresado

--- python_practica1.py
+++ python_practica1.py
@@ -1,24 +1,33 @@
 import random
 numero_secreto = random.randint(1,50)
 
-acumulador = 0
+def numero_valido():
+ while True:
+  try:
+   numero_solicitado = input ("ingrese numero del 1 al 50: ")
+   numero_correcto = int(numero_solicitado)
+
+   if (numero_correcto >= 1) and (numero_correcto <= 50):
+    return numero_correcto
+   else:
+    print ("Debe ingresar un numero entre 1 y 50. Intente nuevamente")
+
+  except ValueError:
+   print ("Error. Debe ingresar un numero entero.")
 
 for i in range (5):
  
- numero_ingresado = int(input ( "ingrese numero del 1 al 50: "))
+  numero_ingresado = numero_valido()
  
- acumulador+= numero_ingresado 
- 
- if numero_secreto == numero_ingresado:
-  
-  print ("Acerto el numero, fin del juego")
-  break
+  if numero_secreto == numero_ingresado:
+   print ("Acerto el numero, fin del juego")
+   break
 
- elif numero_secreto > numero_ingresado:
-  print ( "El numero es mayor")
+  elif numero_secreto > numero_ingresado:
+    print ( "El numero es mayor")
 
- elif numero_secreto < numero_ingresado:
-  print ( "El numero es menor")
+  elif numero_secreto < numero_ingresado:
+    print ( "El numero es menor")
 
 else:
  print (f"Se terminaron los intentos, perdiste. El numero era {numero_secreto}")
